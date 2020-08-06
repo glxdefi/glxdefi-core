341b4598aff4a7b495d8c56e2357c5e8cab10f95
xuyulin <xuyulinnn@sparkpool.com>
2020-08-06 17:16:12 +0800

    GLXToken合约

--- test/GLXToken.test.js
+++ test/GLXToken.test.js
@@ -1,4 +1,10 @@
 'use strict';
+const {
+    BN,           // Big Number support
+    constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
 const { TestHelper } = require('@openzeppelin/cli');
 var GLXToken = artifacts.require("GLXToken");
 
@@ -6,22 +12,24 @@ const { expect } = require('chai');
 
 contract('GLXToken', function (accounts) {
 
+    const [sender, receiver] =  accounts;
+
     beforeEach(async function() {
-        var bcToken = await GLXToken.deployed()
-        console.log(bcToken.address)
-        //console.log(accounts)
+        //合约实例
+        this.gLXToken = await GLXToken.deployed()
     })
 
-    it('test info', async function () {
+    it('should set base info', async function () {
+        await this.gLXToken.setMaker(sender)
+        expect(sender).to.equal(await this.gLXToken.maker.call())
 
-        var bctokenInstance;
+        const beforeBalance = await this.gLXToken.balanceOf.call(receiver)
+        expect(beforeBalance.toNumber()).equal(0);
 
-        return GLXToken.deployed().then(function (instance) {
-            bctokenInstance = instance;
-            console.log(bctokenInstance.name.name)
-            return bctokenInstance.name.call();
-        }).then(function (name) {
-            expect(name).to.equal('GLXToken');
-        })
+        await this.gLXToken.makeCoin(receiver, 1000);
+
+        const afterBalance = await this.gLXToken.balanceOf.call(receiver)
+        expect(afterBalance.toNumber()).equal(1000);
     })
+
 })
