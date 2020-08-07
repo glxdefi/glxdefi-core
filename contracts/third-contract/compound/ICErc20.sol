44f80507e75bfff4ba189ec9844acea16f463220
xuyulin <xuyulinnn@sparkpool.com>
2020-08-07 11:42:24 +0800

    FinCompound.sol合约优化

--- contracts/third-contract/compound/ICErc20.sol
+++ contracts/third-contract/compound/ICErc20.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.6.0;
 
 interface ICErc20 {
+
+    function balanceOf(address account) external view returns (uint256);
+
     function mint(uint256) external returns (uint256);
 
     function exchangeRateCurrent() external returns (uint256);
