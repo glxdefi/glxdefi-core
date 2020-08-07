9c10bf913a25c95e023a0079cd5cdc8856faf4c0
阳乐 <spring.water@foxmail.com>
2020-08-07 11:45:54 +0800

    完善合约 4

--- contracts/GLXGame.sol
+++ contracts/GLXGame.sol
@@ -14,17 +14,25 @@ contract GLXGame is IGLXGame, Lifecycle{
     address public router;
 
     bool public isOnChainGame;
+    //游戏输赢结果，true 表示 正方赢，false表示反方赢
     bool public gameResult;
 
+    //正方总押注额度
     uint256 public trueTotalAmount;
+    //反方总押注额度
     uint256 public falseTotalAmount;
+    //正方总参与人数
     uint256 public trueTotalCount;
+    //反方总参与人数
     uint256 public falseTotalCount;
 
+    //正方押注额度
     mapping (address => uint256) public trueAmountMap;
+    //反方押注额度
     mapping (address => uint256) public falseAmountMap;
 
-    mapping (address => bool) public receivedMap;
+    //领奖状态：false表示未领取，true表示已经领取
+    mapping (address => bool) public isReceivedMap;
 
     address public maxAmount;
     address public maxAmountAccount;
@@ -82,7 +90,7 @@ contract GLXGame is IGLXGame, Lifecycle{
 
     function receive(address account) external lock whenEnded returns (bool) {
         require(account != address(0), "GLXGame: RECEIVE_ADDRESS_ZERO");
-        require(!receivedMap[account], "GLXGame: RECEIVED");
+        require(!isReceivedMap[account], "GLXGame: RECEIVED");
 
         require(trueTotalAmount != 0, "GLXGame: TRUE_TOTAL_AMOUNT_ZERO");
         require(falseTotalAmount != 0, "GLXGame: FALSE_TOTAL_AMOUNT_ZERO");
@@ -93,7 +101,7 @@ contract GLXGame is IGLXGame, Lifecycle{
             uint64 receiveAmount = GLXHelper.calReceiveAmount(trueAmountMap[account], trueTotalAmount, falseTotalAmount);
             GLXHelper.safeTransfer(account, receiveAmount);
 
-            receivedMap[account] = true;
+            isReceivedMap[account] = true;
 
         } else {
             require(falseAmountMap[account] != 0, "GLXGame: NOT_EXIST");
@@ -101,7 +109,7 @@ contract GLXGame is IGLXGame, Lifecycle{
             uint64 receiveAmount = GLXHelper.calReceiveAmount(falseAmountMap[account], falseTotalAmount, trueTotalAmount);
             GLXHelper.safeTransfer(account, receiveAmount);
 
-            receivedMap[account] = true;
+            isReceivedMap[account] = true;
         }
 
         if (account == maxAmountAccount) {
