f96c0ccd912c957b234aff61e235d396c58c81c7
xuyulin <xuyulinnn@sparkpool.com>
2020-08-08 10:05:17 +0800

    abi

--- test/Game.test.js
+++ test/Game.test.js
@@ -21,12 +21,12 @@ contract('Game', function (accounts) {
 
     beforeEach(async function() {
         //合约实例
-        // this.dai = await MockErc20.deployed()
-        // this.hope = await GLXToken.deployed()
-        // this.cdai = await MockCErc20.deployed()
-        // this.router = await GLXRouter.deployed()
-        // this.gLXFactory = await GLXFactory.deployed()
-        // // console.log('factory: ' + this.gLXFactory.address)
+        this.dai = await MockErc20.deployed()
+        this.hope = await GLXToken.deployed()
+        this.cdai = await MockCErc20.deployed()
+        this.router = await GLXRouter.deployed()
+        this.gLXFactory = await GLXFactory.deployed()
+        // console.log('factory: ' + this.gLXFactory.address)
         // console.log('router: ' + this.router.address)
         // console.log('cdai: ' + this.cdai.address)
         // console.log('hope: ' + this.hope.address)
@@ -35,22 +35,16 @@ contract('Game', function (accounts) {
 
     it('should createGame', async function () {
 
-        this.gLXFactory=  await GLXFactory.at("0x4E8c0faA057Bc6aeDBFd88420D4785e3125CA51B")
-        console.log(this.gLXFactory.address)
-
-        const result = await this.gLXFactory.createGame('0x3eBD5291F974E943E18c037960b3816cDcDd553b',
-            '0x4969D629Fa7AAaC886D3326CaD983eaE8CE3E6AC', '0xE0bEA5898d8f1C680bB64F8bc09B95f17bC98f7C',
-            '0xAc8f813A100f316528c9F6781e081e6f055E26aE', '0xAc8f813A100f316528c9F6781e081e6f055E26aE',
-            8452371, 8452471, true, '0x4969D629Fa7AAaC886D3326CaD983eaE8CE3E6AC', 1000)
+        const result = await this.gLXFactory.createGame(this.router.address, this.dai.address, this.hope.address,
+            this.cdai.address, this.cdai.address, 10000, 10100, true, this.dai.address, 1000)
         const gameAddress = result.receipt.logs[0].args.a
-        console.log(gameAddress)
-        //
-        // const gameExtToken = await this.gLXFactory.getGameExtToken.call(gameAddress)
-        // expect(gameExtToken).equal(this.dai.address);
-        //
-        // this.game = await GLXGame.at(gameAddress)
-        //
-        // expect(await this.game.factory.call()).equal(this.gLXFactory.address)
+
+        const gameExtToken = await this.gLXFactory.getGameExtToken.call(gameAddress)
+        expect(gameExtToken).equal(this.dai.address);
+
+        this.game = await GLXGame.at(gameAddress)
+
+        expect(await this.game.factory.call()).equal(this.gLXFactory.address)
     })
     it('should startGame', async function() {
     })
