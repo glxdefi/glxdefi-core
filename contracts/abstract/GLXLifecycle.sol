e97c915c2d7a2340d889082b0c7ee3f90f7d88d0
阳乐 <spring.water@foxmail.com>
2020-08-11 20:15:03 +0800

    add comment

--- contracts/abstract/GLXLifecycle.sol
+++ contracts/abstract/GLXLifecycle.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/GSN/Context.sol";
 
 
+//game的生命周期状态内
  contract GLXLifecycle is Context {
 
     uint public startBlockNumber;
@@ -11,6 +12,7 @@ import "@openzeppelin/contracts/GSN/Context.sol";
     uint public endBlockNumber;
 
 
+     //初始化游戏起始高度，结束高度
     function _initBlockNumber(uint _startBlockNumber, uint _endBlockNumber)  internal  {
         require(startBlockNumber == 0 && endBlockNumber == 0, "GLXLifecycle: BLOCK_NUMBER_ALREADY_INIT");
         require(_startBlockNumber < _endBlockNumber, "GLXLifecycle: BLOCK_NUMBER_INVALID");
@@ -20,6 +22,7 @@ import "@openzeppelin/contracts/GSN/Context.sol";
         endBlockNumber = _endBlockNumber;
     }
 
+     //游戏是否已经开始
     function isStarted() public view returns (bool) {
         if(block.number >= startBlockNumber) {
             return true;
@@ -28,6 +31,7 @@ import "@openzeppelin/contracts/GSN/Context.sol";
     }
 
 
+     //游戏是否已经开奖了
     function isEnded() public view returns (bool) {
         if(block.number > endBlockNumber) {
             return true;
